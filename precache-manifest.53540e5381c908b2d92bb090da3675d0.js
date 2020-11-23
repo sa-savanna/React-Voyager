05e3558563f4f20052ae38071a2411c13a2507eb
sa-savanna <savanna.be.ua@gmail.com>
2020-11-23 12:45:57 +0100

    Updates

---
+++ precache-manifest.53540e5381c908b2d92bb090da3675d0.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07f7c3d42d20a4602ea99fa4ea184697",
+    "url": "/sa-savanna/React-Voyager.git/index.html"
+  },
+  {
+    "revision": "de32cd9e822cf7eada87",
+    "url": "/sa-savanna/React-Voyager.git/static/css/2.756c529d.chunk.css"
+  },
+  {
+    "revision": "9b00bd30b6001f99d702",
+    "url": "/sa-savanna/React-Voyager.git/static/css/main.39b3b70c.chunk.css"
+  },
+  {
+    "revision": "de32cd9e822cf7eada87",
+    "url": "/sa-savanna/React-Voyager.git/static/js/2.5c1245d7.chunk.js"
+  },
+  {
+    "revision": "622fffac73e56111dcf3fea0a2e83cfb",
+    "url": "/sa-savanna/React-Voyager.git/static/js/2.5c1245d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b00bd30b6001f99d702",
+    "url": "/sa-savanna/React-Voyager.git/static/js/main.2fcfa34d.chunk.js"
+  },
+  {
+    "revision": "3435a1d262507e7da94e",
+    "url": "/sa-savanna/React-Voyager.git/static/js/runtime-main.543c8fa4.js"
+  },
+  {
+    "revision": "4987e8ac4dc823e51fd62e1d510e0956",
+    "url": "/sa-savanna/React-Voyager.git/static/media/Brazil.4987e8ac.jpg"
+  },
+  {
+    "revision": "96d56c4568fbf6c1956bc32797ad0631",
+    "url": "/sa-savanna/React-Voyager.git/static/media/France.96d56c45.jpg"
+  },
+  {
+    "revision": "8fdbb3a701400acfdc8a0e7d97149c7b",
+    "url": "/sa-savanna/React-Voyager.git/static/media/Japan_1920.8fdbb3a7.jpg"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/sa-savanna/React-Voyager.git/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/sa-savanna/React-Voyager.git/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "935e4756d1ae35e17a8daa1c5908bbdb",
+    "url": "/sa-savanna/React-Voyager.git/static/media/brussels-1920.935e4756.jpg"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/sa-savanna/React-Voyager.git/static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/sa-savanna/React-Voyager.git/static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/sa-savanna/React-Voyager.git/static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/sa-savanna/React-Voyager.git/static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/sa-savanna/React-Voyager.git/static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/sa-savanna/React-Voyager.git/static/media/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/sa-savanna/React-Voyager.git/static/media/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/sa-savanna/React-Voyager.git/static/media/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/sa-savanna/React-Voyager.git/static/media/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/sa-savanna/React-Voyager.git/static/media/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/sa-savanna/React-Voyager.git/static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/sa-savanna/React-Voyager.git/static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/sa-savanna/React-Voyager.git/static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/sa-savanna/React-Voyager.git/static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/sa-savanna/React-Voyager.git/static/media/fa-solid-900.ee09ad75.woff"
+  },
+  {
+    "revision": "79f9e07ea081ea14096541b2d80c2f09",
+    "url": "/sa-savanna/React-Voyager.git/static/media/globe_PNG62.79f9e07e.png"
+  },
+  {
+    "revision": "00c7443ede94b711a90f2433d45d9e58",
+    "url": "/sa-savanna/React-Voyager.git/static/media/india_1920.00c7443e.jpg"
+  },
+  {
+    "revision": "b9c5579574d1e29d15d6310f39d75fff",
+    "url": "/sa-savanna/React-Voyager.git/static/media/ukraine-kyiv_1920.b9c55795.jpg"
+  }
+]);
